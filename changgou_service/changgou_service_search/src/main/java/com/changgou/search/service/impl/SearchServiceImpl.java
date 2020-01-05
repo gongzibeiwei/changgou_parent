bb93c36145d5a643240c4e082946859497027ab2
jaime <jaime_zhao@163.com>
2020-01-05 22:24:21 +0800

    完成基于ElasticSearch的商品搜索功能

--- changgou_service/changgou_service_search/src/main/java/com/changgou/search/service/impl/SearchServiceImpl.java
+++ changgou_service/changgou_service_search/src/main/java/com/changgou/search/service/impl/SearchServiceImpl.java
@@ -10,7 +10,14 @@ import org.elasticsearch.index.query.Operator;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.bucket.terms.StringTerms;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.core.SearchResultMapper;
@@ -19,10 +26,8 @@ import org.springframework.data.elasticsearch.core.aggregation.impl.AggregatedPa
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author Jaime
@@ -50,11 +55,81 @@ public class SearchServiceImpl implements SearchService {
             NativeSearchQueryBuilder nativeSearchQueryBuilder = new NativeSearchQueryBuilder();
             BoolQueryBuilder boolQuery = QueryBuilders.boolQuery();
 
-            //按照关键字查询
+            //按照关键字进行过滤查询
             if (StringUtils.isNotEmpty(searchMap.get("keywords"))) {
+                //matchQuery模糊查询
                 boolQuery.must(QueryBuilders.matchQuery("name", searchMap.get("keywords")).operator(Operator.AND));
             }
+
+            //按照品牌进行过滤查询
+            if (StringUtils.isNotEmpty(searchMap.get("brand"))) {
+                //termQuery全字匹配
+                boolQuery.filter(QueryBuilders.termQuery("brandName", searchMap.get("brand")));
+            }
+
+            //按照规格进行过滤查询
+            for (String key : searchMap.keySet()) {
+                if (key.startsWith("spec_")) {
+                    String value = searchMap.get(key).replace("%2B", "+");
+                    //spec_网络制式，截取spec_
+                    boolQuery.filter(QueryBuilders.termQuery(("specMap." + key.substring(5) + ".keyword"), value));
+                }
+            }
+
+            //按照价格进行区间过滤查询
+            if (StringUtils.isNotEmpty(searchMap.get("price"))) {
+                String[] prices = searchMap.get("price").split("-");
+                //0-500 500-1000
+                if (prices.length == 2) {
+                    boolQuery.filter(QueryBuilders.rangeQuery("price").lte(prices[1]));
+                }
+                boolQuery.filter(QueryBuilders.rangeQuery("price").gte(prices[0]));
+            }
+
             nativeSearchQueryBuilder.withQuery(boolQuery);
+
+            //按照品牌进行分组（聚合）查询
+            String skuBrand = "skuBrand";
+            //terms进行聚合查询后的分组字段列名，field对ES哪一个域进行操作
+            nativeSearchQueryBuilder.addAggregation(AggregationBuilders.terms(skuBrand).field("brandName"));
+
+            //按照规格进行分组（聚合）查询
+            String skuSpec = "skuSpec";
+            //terms进行聚合查询后的分组字段列名，field对ES哪一个域进行操作
+            nativeSearchQueryBuilder.addAggregation(AggregationBuilders.terms(skuSpec).field("spec.keyword"));
+
+            //开启分页查询
+            String pageNum = searchMap.get("pageNum");//当前页
+            String pageSize = searchMap.get("pageSize");//每页显示记录数
+            if (StringUtils.isEmpty(pageNum)) {
+                pageNum = "1";
+            }
+            if (StringUtils.isEmpty(pageSize)) {
+                pageSize = "30";
+            }
+            //设置分页
+            //第一个参数：当前页
+            //第二个参数：每页显示记录数
+            nativeSearchQueryBuilder.withPageable(PageRequest.of((Integer.parseInt(pageNum) - 1), Integer.parseInt(pageSize)));
+
+            //按照相关字段进行排序查询
+            //1.当前域  2.当前的排序操作（升序ASC，降序DESC）
+            if (StringUtils.isNotEmpty(searchMap.get("sortField")) && StringUtils.isNotEmpty(searchMap.get("sortRule"))) {
+                if ("ASC".equals(searchMap.get("sortRule"))) {
+                    //升序
+                    nativeSearchQueryBuilder.withSort(SortBuilders.fieldSort(searchMap.get("sortField")).order(SortOrder.ASC));
+                } else {
+                    //降序
+                    nativeSearchQueryBuilder.withSort(SortBuilders.fieldSort(searchMap.get("sortField")).order(SortOrder.DESC));
+                }
+            }
+
+            //设置高亮域以及高亮的样式
+            HighlightBuilder.Field field = new HighlightBuilder.Field("name")//高亮域
+                    .preTags("<span style='color:red'>")//高亮样式的前缀
+                    .postTags("</span>");//高亮样式的后缀
+            nativeSearchQueryBuilder.withHighlightFields(field);
+
             //开启查询
             /**
              * 第一个参数: 条件构建对象
@@ -75,6 +150,14 @@ public class SearchServiceImpl implements SearchService {
                         for (SearchHit hit : hits) {
                             //SearchHit转换为skuinfo
                             SkuInfo skuInfo = JSON.parseObject(hit.getSourceAsString(), SkuInfo.class);
+
+                            //进行高亮操作
+                            Map<String, HighlightField> highlightFields = hit.getHighlightFields();
+                            if (highlightFields != null && highlightFields.size() > 0) {
+                                //替换数据
+                                skuInfo.setName(highlightFields.get("name").getFragments()[0].toString());
+                            }
+
                             list.add((T) skuInfo);
                         }
                     }
@@ -88,6 +171,20 @@ public class SearchServiceImpl implements SearchService {
             resultMap.put("totalPages", resultInfo.getTotalPages());
             //数据集合
             resultMap.put("rows", resultInfo.getContent());
+
+            //封装品牌的分组结果
+            StringTerms brandTerms = (StringTerms) resultInfo.getAggregation(skuBrand);
+            List<String> brandList = brandTerms.getBuckets().stream().map(bucket -> bucket.getKeyAsString()).collect(Collectors.toList());
+            resultMap.put("brandList", brandList);
+
+            //封装规格的分组结果
+            StringTerms specTerms = (StringTerms) resultInfo.getAggregation(skuSpec);
+            List<String> specList = specTerms.getBuckets().stream().map(bucket -> bucket.getKeyAsString()).collect(Collectors.toList());
+            resultMap.put("specList", specList);
+
+            //封装当前页
+            resultMap.put("pageNum", pageNum);
+
             return resultMap;
         }
         return null;
